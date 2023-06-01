a5eb0263dd0992975263f5898d26b34bbee6b11a
Dan Heslinga <dan@heslinga.us>
2023-05-31 17:01:33 -1000

    before subscribing to new link elements

--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
 const { PrismaClient } = require("@prisma/client");
+const { PubSub } = require('apollo-server')
+
+const pubsub = new PubSub()
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent, args, context) => {
-			return context.prisma.link.findMany()
-		},
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-				data: {
-					url: args.url,
-					description: args.description,
-				},
-			})
-			return newLink
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const prisma = new PrismaClient();
 
+const { getUserId } = require('./utils');
+
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  typeDefs: fs.readFileSync(
+		path.join(__dirname, "schema.graphql"), 
+		"utf8"
+	),
   resolvers,
-  context: {
-    prisma,
-  },
+  context: ({ req }) => {
+		return {
+      ...req,
+	    prisma,
+			pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };  
+	}
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
